d3bc295b6efec116c327a510c17ecc44e3098177
Steven Wade <steven@stevenwade.co.uk>
2016-04-08 14:54:22 +0100

    laying out the "AWS api call" to be more readable.

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@
 #sudo $(which chef) exec rake ci
 
 ## Obtain the source AMI to use for our Packer build.
-base_ami_id=$(aws ec2 describe-images --filters Name=tag-key,Values=name Name=tag-value,Values=linux-ubuntu-base --output text --query 'Images[0].ImageId')
+base_ami_id=$(aws ec2 describe-images \
+--filters Name=tag-key,Values=name Name=tag-value,Values=linux-ubuntu-base \
+--output text \
+--query 'Images[0].ImageId' \
+)
 
 ## Build an AMI for this cookbook
 $(which chef) exec rake packer[$base_ami_id]
