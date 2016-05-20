f266c5ed192bb8319fd7ac68a5b4b9ad6dd3f82e
James McKay <mckayj@parliament.uk>
2016-05-20 16:52:49 +0100

    Install Docker Compose

--- files/configure_docker.sh
+++ files/configure_docker.sh
@@ -1,5 +1,12 @@
+# ====== Configure Docker user ====== #
+
 sudo groupadd docker
 sudo usermod -G docker go
 sudo service docker restart
 
 echo "Done."
+
+# ====== Install Docker Compose ====== #
+
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
